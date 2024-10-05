5f8bfa8506dd1e49f8fc2b99010a005e6cd4d6a1
YASH FADADU <fadaduyash1994@gmail.com>
2024-10-05 18:58:17 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,11 +1,21 @@
 from flask import Flask
 import os
+import threading
 
 app = Flask(__name__)
 
+counter = 0
+
+def increment_counter():
+    global counter
+    counter += 1
+    threading.Timer(30.0, increment_counter).start()
+
+increment_counter()
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return f"Current Counter is {counter}"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
